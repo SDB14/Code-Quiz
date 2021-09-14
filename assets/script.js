8a5d57de99d7b53096125f9d259dfe574071ac14
SDB14 <cwruboot22@gmail.com>
2021-09-14 17:13:02 -0400

    start

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// Global Variables that point to relavant classes or ids in HTML
+
+//Variables that keep track of quiz time / state
+
+// Creat a start function that when clicked it starts timer and displays first question
+
+// Create a function that displays and cycles through questions
+
+// Create a function that determines if answer is correct or incorrect and if incorrect it will subtracts time from the clock
+
+// Set time equal score
+
+// Create an end quiz function that is set off when timer reaches zero or no more questions are available
+
+// Create a function that when game is over it transitions to local storage for initials
